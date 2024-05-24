d9666bb73eb42cb7aac39b0a17f95fa35faf58fa
AK_333 <connect.akashkakadiya@gmail.com>
2024-05-24 11:16:39 +0530

    CRUD using axios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import App24 from './Axios/App24';
+import reportWebVitals from "./reportWebVitals";
+import App24 from "./Axios/App24";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <App24/>
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* run this for basic CRUD */}
+        {/* <App /> */}
+
+        {/* run this for axios CRUD*/}
+        <App24 />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
